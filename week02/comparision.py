55ff18f5f3cbe1e4a22cd62d78a8984f3021bd94
zhangmingxunfff-spec <zhangmingxunfff@gmail.com>
2026-04-25 17:55:22 +0700

    Datatype in pthon

---
+++ week02/comparision.py
@@ -0,0 +1,28 @@
+# Comaprision in python
+# > greater than
+# < less than
+# >= greater or equal
+# <= less than or equal
+# == equal
+# != not equal
+
+a = 100
+b= 200
+c = 300
+d = 200
+
+print(a>b)
+# Data type to boolean
+
+
+fruit = ['apple', 'banana', 'cherry', 'orange']
+print(fruit[-1])
+
+fruit[1] = "coconut"
+print(fruit)
+
+fruit.append("watermelon")
+print(fruit)
+
+fruit.remove("cherry")
+print(fruit)
---
+++ week02/week02.py
@@ -0,0 +1,7 @@
+a = "I've"
+b = 'I'
+
+
+
+personal = ["firstName" , 7, True, 'LastName']
+print(personal[3])
