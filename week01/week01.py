8e99e6da386800217006c75a2a7a690e5e7f2483
zhangmingxunfff-spec <zhangmingxunfff@gmail.com>
2026-04-25 16:41:03 +0700

    Data type in python

---
+++ week01/week01.py
@@ -0,0 +1,41 @@
+# Set up environment in my device
+#declare a variable
+# 1. variable name
+# 2. = (assignment)
+# 3. value default
+
+x = 10
+
+# warning!
+# ไม่สามารถประกาศเป็นตัวเลขได้
+# ไม่สามารถประกาศเป็นสัญลักษณ์ได้(-ไม่ได้)(_ได้)
+# ไม่สามารถประกาศเป็นเว้นได้
+# ตัวเลขใช้ได้แต่ต้องอยู๋หน้าตัวอักษร
+# 1x = 10 x
+# my-var = 10 x
+# my var = 10 x
+
+
+
+
+# data type in python
+# str = string " " , ' '
+
+# numberic
+# int = จำนวนเต็ม 12345
+# float = ทศนิยม .12345
+
+# bool = การตัดสินใจ true , false (true = 1 , false = 0)
+# list = [ ]
+
+fisrtname = "wachirawit"
+lastName = ' mool-Ang'
+
+x = 100.12345
+
+isAdult = True
+
+fullname = (fisrtname) + ' ' + ( lastName) 
+
+print(fullname)
+
